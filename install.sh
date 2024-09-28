f248da082eedb1f7c9bc0c70bd0ecb11f814bc51
matias pizarro <matias@pizarro.net>
2024-09-29 00:54:57 +0200

    Improve output and expand sample .gitignore file

--- install.sh
+++ install.sh
@@ -2,16 +2,21 @@
 
 /usr/bin/install -v -m 0755 ./cfg /usr/local/bin
 mkdir -p /usr/local/share/cfg
-/usr/bin/install -v -m 0644 ./cfg.conf.sample /usr/local/share/cfg/cfg.conf.sample
+
+/usr/bin/install -v -m 0644 ./cfg.conf.sample \
+    /usr/local/share/cfg/cfg.conf.sample
 if [ ! -f /usr/local/etc/cfg.conf ]; then
   cp -p /usr/local/share/cfg/cfg.conf.sample /usr/local/etc/cfg.conf
   echo "install: ./cfg.conf.sample -> /usr/local/etc/cfg.conf"
 fi
-/usr/bin/install -v -m 0644 ./box.gitignore.sample /usr/local/share/cfg
+
+/usr/bin/install -v -m 0644 ./host.gitignore.sample \
+    /usr/local/share/cfg/host.gitignore.sample
 if [ ! -f /.gitignore ]; then
-  cp -p /usr/local/share/cfg/box.gitignore.sample /.gitignore
-  echo "install: ./box.gitignore.sample -> /.gitignore"
+  cp -p /usr/local/share/cfg/host.gitignore.sample /.gitignore
+  echo "install: ./host.gitignore.sample -> /.gitignore"
 fi
+
 gzip --to-stdout ./cfg.8 | dd of=/usr/local/share/man/man8/cfg.8.gz 1> /dev/null    2> /dev/null
 echo "install: ./cfg.8 -> /usr/local/share/man/man8/cfg.8.gz"
 echo ""
--- uninstall.sh
+++ uninstall.sh
@@ -2,10 +2,16 @@
 
 rm -f /usr/local/bin/cfg
 echo "deleted: /usr/local/bin/cfg"
-rm -f /usr/local/etc/cfg.conf.sample
-echo "deleted: /usr/local/etc/cfg.conf.sample"
+
+rm -f /usr/local/share/cfg/cfg.conf.sample
+echo "deleted: /usr/local/share/cfg/cfg.conf.sample"
+
+rm -f /usr/local/share/cfg/host.gitignore.sample
+echo "deleted: /usr/local/share/cfg/host.gitignore.sample"
+
 rm -f /usr/local/share/man/man8/cfg.8.gz
 echo "deleted: /usr/local/share/man/man8/cfg.8.gz"
+
 if [ -f "/usr/local/etc/cfg.conf" ]; then
   echo "warning: /usr/local/etc/cfg.conf has been left untouched"
 fi
